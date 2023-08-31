383c7102b1fc1180d214c643c1d4911937c31467
dioguest <diogosouzasoaress@gmail.com>
2023-08-31 15:48:01 -0300

    exercicio 1

---
+++ atividade2/exercicio1.js
@@ -0,0 +1,15 @@
+// Declaração de variáveis numéricas
+var numA = 10;
+var numB = 5;
+
+// Operações e armazenamento dos resultados
+var sumResult = numA + numB;
+var subResult = numA - numB;
+var mulResult = numA * numB;
+var divResult = numA / numB;
+
+// Impressão dos resultados
+console.log("Resultado da soma entre " + numA + " e " + numB + " = " + sumResult);
+console.log("Resultado da subtração entre " + numA + " e " + numB + " = " + subResult);
+console.log("Resultado da multiplicação entre " + numA + " e " + numB + " = " + mulResult);
+console.log("Resultado da divisão entre " + numA + " e " + numB + " = " + divResult);
